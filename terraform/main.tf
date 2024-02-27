37031b7ba111bfb89a2ed61276e3194ccb7cc257
assocyate <94201240+assocyate@users.noreply.github.com>
2024-02-27 12:48:58 -0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,10 +7,39 @@ terraform {
   }
 
   required_version = ">=0.14.9"
-
+   backend "s3" {
+       bucket = "tfstate-gitlab"
+       key    = "terraform.tfstate"
+       region = "us-west-2"
 }
 
 provider "aws" {
   version = "~>3.0"
   region  = "us-west-2"
+}
+
+resource "aws_s3_bucket" "s3Bucket" {
+     bucket = "tfstate-gitlab"
+     acl       = "public-read"
+
+     policy  = <<EOF
+{
+     "id" : "MakePublic",
+   "version" : "2012-10-17",
+   "statement" : [
+      {
+         "action" : [
+             "s3:GetObject"
+          ],
+         "effect" : "Allow",
+         "resource" : "arn:aws:s3:::tfstate-gitlab/*",
+         "principal" : "*"
+      }
+    ]
+  }
+EOF
+
+   website {
+       index_document = "index.html"
+   }
 }
