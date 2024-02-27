fae925d684be1c5da8156ac6bea93c065fe0c4f5
assocyate <94201240+assocyate@users.noreply.github.com>
2024-02-27 14:11:06 -0800

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,44 +2,23 @@ terraform {
   required_providers {
     aws = {
       source = "hashicorp/aws"
-      Version = "~>3.27"
     }
   }
 
-  required_version = ">=0.14.9"
-   backend "s3" {
-       bucket = "tfstate-gitlab"
-       key    = "terraform.tfstate"
-       region = "us-west-2"
+  backend "s3" {
+    region = "us-west-2"
+    key    = "terraform.tfstate"
+  }
 }
 
 provider "aws" {
-  version = "~>3.0"
-  region  = "us-west-2"
+  region = "us-west-2"
 }
 
-resource "aws_s3_bucket" "s3Bucket" {
-     bucket = "tfstate-gitlab"
-     acl       = "public-read"
-
-     policy  = <<EOF
-{
-     "id" : "MakePublic",
-   "version" : "2012-10-17",
-   "statement" : [
-      {
-         "action" : [
-             "s3:GetObject"
-          ],
-         "effect" : "Allow",
-         "resource" : "arn:aws:s3:::tfstate-gitlab/*",
-         "principal" : "*"
-      }
-    ]
+resource "aws_instance" "test_instance" {
+  ami           = "ami-830c94e3"
+  instance_type = "t2.nano"
+  tags = {
+    Name = "test_instance"
   }
-EOF
-
-   website {
-       index_document = "index.html"
-   }
 }
